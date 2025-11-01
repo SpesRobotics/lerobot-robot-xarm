e54b4da2d3b2b5ec289dfe6532f01d6f73152d34
Darko Lukic <lukicdarkoo@gmail.com>
2025-11-01 22:36:34 +0100

    markdown description

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
+from anyio import Path
 from setuptools import setup, find_packages
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="lerobot_robot_xarm",
-    version="0.0.1",
+    version="0.0.2",
     description="LeRobot XArm integration",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Spes Robotics",
     author_email="contact@spes.ai",
     packages=find_packages(),
@@ -12,7 +18,7 @@ setup(
         "numpy",
         "transforms3d",
         "teleop",
-        "lerobot",
+        "lerobot>=0.4",
     ],
     python_requires=">=3.10",
     classifiers=[
